6500d5dafd161cacc415b5ec1b812df19417893b
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-03-19 12:33:51 +0100

    es necesario que el numero introducido sea primo, si no lo es uso el siguiente

--- practica1/src/ejercicio6_1.cpp
+++ practica1/src/ejercicio6_1.cpp
@@ -16,10 +16,16 @@ int main(int argc, char ** argv) {
 	mp::cpp_int a(argv[1]);
 	mp::cpp_int p (argv[2]);
 
+	if ( !es_primo(p) ){
+		p = siguiente_primo(p);
+		std::cout << "Aviso: El número introducido no es primo, utilizaré el siguiente primo: " << p << std::endl;
+	}
+
 	mp::cpp_int resultado = raiz_cuadrada_mod(a, p);
 
 	if ( resultado != -1 ){
 		std::cout << "La raiz cuadrada de " << a << " modulo " << p << " es : " << resultado << std::endl;
+		std::cout << "Comprobación (utilizando powm): " << mp::powm(resultado, 2, p) << std::endl;
 	} else {
 		std::cout << a << " no tiene raiz cuadrada modulo " << p << std::endl;
 	}
