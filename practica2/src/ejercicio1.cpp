80f3ebbc90bbc45cecf59ed5a597bca3eff4da2d
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-04-08 11:22:51 +0200

    main del ejercicio 1

---
+++ practica2/src/ejercicio1.cpp
@@ -0,0 +1,49 @@
+
+#include "funciones.hpp"
+
+#include <iostream>
+#include <boost/dynamic_bitset.hpp>
+
+
+int main ( int argc, char ** argv) {
+
+	if ( argc != 2) {
+		std::cerr << "ERROR: Se necesita un parámetro.\n"
+					 << "\t Uso: " << argv[0] << " <secuencia_bits>" << std::endl;
+		exit(-1);
+	}
+
+	// para ejemplo: 000100110101111 si cumple todos
+
+
+	std::string bits_entrada = std::string(argv[1]);
+
+	boost::dynamic_bitset<> bitset(bits_entrada);
+
+	bool cumple_primer_postulado = cumple_primer_postulado_golomb(bitset);
+	bool cumple_segundo_postulado = cumple_segundo_postulado_golomb(bitset);
+	bool cumple_tercer_postulado = cumple_tercer_postulado_golomb(bitset);
+
+	bool cumple_postulados = cumple_postulados_golomb(bitset);
+
+	std::cout << "La secuencia de bits: " << bits_entrada;
+
+	if ( !cumple_postulados ) {
+		std::cout << " NO ";
+	}
+
+	std::cout << " cumple los postulados de Golomb" << std::endl;
+
+	if ( cumple_primer_postulado ) {
+		std::cout << "Cumple el primer postulado" << std::endl;
+	}
+
+	if ( cumple_segundo_postulado ) {
+		std::cout << "Cumple el segundo postulado" << std::endl;
+	}
+
+	if ( cumple_tercer_postulado ) {
+		std::cout << "Cumple el tercer postulado" << std::endl;
+	}
+
+}
