0f94c6c06169700606e4b4a433ed6efe6e379eac
advy99 <advy99@gmail.com>
2021-05-13 11:48:37 +0200

    main del ejercicio 1

--- practica3/src/ejercicio1.cpp
+++ practica3/src/ejercicio1.cpp
@@ -7,14 +7,21 @@
 
 int main(int argc, char ** argv) {
 
-	if ( argc != 2 ) {
-		std::cout << "Error: Ejecutar con un parametro: <longitud_llave>" << std::endl;
+	if ( argc != 3 ) {
+		std::cout << "Error: Ejecutar con un parametro: <longitud_llave> <mensaje_cifrar>" << std::endl;
 		exit(-1);
 	}
 
-	int longitud = atoi(argv[1]);
+	unsigned longitud = atoi(argv[1]);
+	std::string string_mensaje(argv[2]);
 
-	auto resultado = funcion_mochila(longitud);
+	if ( longitud != string_mensaje.size() ) {
+		std::cout << "Error: El mensaje y la longitud de clave tienen que ser iguales." << std::endl
+					 << "\t Tamaños dados: " << longitud << " y " << string_mensaje.size() << std::endl;
+		exit(-2);
+	}
+
+	auto resultado = generar_llaves(longitud);
 
 	std::vector<mp::cpp_int> llave_publica = resultado.first;
 
@@ -41,5 +48,28 @@ int main(int argc, char ** argv) {
 
 	std::cout << "N: " << n_llave_privada << std::endl;
 	std::cout << "U: " << u_llave_privada << std::endl;
+
+	std::vector<bool> mensaje_cifrar;
+
+	mensaje_cifrar.resize(string_mensaje.size());
+
+	for(unsigned i = 0; i < string_mensaje.size(); i++) {
+		// si está puesto a uno, lo ponemos true, si no false
+		mensaje_cifrar[i] = string_mensaje[i] - '0' == 1;
+	}
+
+	mp::cpp_int mensaje_cifrado = cifrar_secuencia_bits(mensaje_cifrar, llave_publica);
+
+	std::cout << "Mensaje cifrado: " << mensaje_cifrado << std::endl;
+
+	std::vector<bool> mensaje_descifrado = descifrar(mensaje_cifrado, llave_privada);
+
+	std::cout << "Mensaje descifrado: ";
+	for ( unsigned i = 0; i < mensaje_descifrado.size(); i++) {
+		std::cout << mensaje_descifrado[i];
+	}
+	std::cout << std::endl;
+	std::cout << "Mensaje       dado: " << string_mensaje << std::endl;
+
 
 }
